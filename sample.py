1443ac1627f9eb774ea8a00b07428e3381195eb0
Tomoki Morimoto <morimoto.ke@gmail.com>
2023-06-06 15:51:35 +0900

    🔥

--- sample.py
+++ sample.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import cx_Oracle
+from pprint import pprint
+import os
+import configparser
 
-# プライマリキーのカラム名
-primary_key_columns = ['MachineSN', 'PlantID']
+config = configparser.ConfigParser()
+ini_file_path = os.path.join(r"./config.ini")
+if os.path.isfile(ini_file_path):
+    config.read(ini_file_path)
+else:
+    raise FileNotFoundError(f"iniファイルが存在しません")
 
 # 社内版データベース接続情報
-internal_username = 'your_internal_username'
-internal_password = 'your_internal_password'
-internal_host = 'your_internal_host'
-internal_port = 'your_internal_port'
-internal_service_name = 'your_internal_service_name'
+internal_username = config["INTERNAL"]["ID"]
+internal_password = config["INTERNAL"]["PW"]
+internal_host = config["INTERNAL"]["HOST"]
+internal_port = config["INTERNAL"]["PORT"]
+internal_service_name = config["INTERNAL"]["SERVICE_NAME"]
 
 # 社外版データベース接続情報
-external_username = 'your_external_username'
-external_password = 'your_external_password'
-external_host = 'your_external_host'
-external_port = 'your_external_port'
-external_service_name = 'your_external_service_name'
+external_username = config["EXTERNAL"]["ID"]
+external_password = config["EXTERNAL"]["PW"]
+external_host = config["EXTERNAL"]["HOST"]
+external_port = config["EXTERNAL"]["PORT"]
+external_service_name = config["EXTERNAL"]["SERVICE_NAME"]
 
 # データベースへの接続とデータの読み込み
 def get_data(username, password, host, port, service_name, table_name):
@@ -29,24 +36,35 @@ def get_data(username, password, host, port, service_name, table_name):
 
 # 各テーブルのデータを比較
 table_name = 'MachineSN'
-
 # 社内版データの取得
 internal_data = get_data(internal_username, internal_password, internal_host, internal_port, internal_service_name, table_name)
-
 # 社外版データの取得
 external_data = get_data(external_username, external_password, external_host, external_port, external_service_name, table_name)
 
-# データのマージと差分の抽出
-merged_data = pd.merge(internal_data, external_data, on=primary_key_columns, how='outer', suffixes=('_internal', '_external'))
-new_data = merged_data[merged_data['MachineSN_external'].isnull()]
-updated_data = merged_data[~merged_data['MachineSN_internal'].equals(merged_data['MachineSN_external'])]
+internal_col_list = list(internal_data.columns) 
+external_col_list = list(external_data.columns) 
+assert internal_col_list == external_col_list, "2つのデータのカラムが異なります"
+col_list = internal_col_list
 
-# 新規追加データの表示
-if len(new_data) > 0:
-    print("新規追加データ:")
-    print(new_data[primary_key_columns])
+internal_data["source"] = "internal"
+external_data["source"] = "external"
+internal_data["key"] = list(zip(internal_data["MACHINESN"], internal_data["PLANTID"]))
+external_data["key"] = list(zip(external_data["MACHINESN"], external_data["PLANTID"]))
 
-# 更新データの表示
-if len(updated_data) > 0:
-    print("更新データ:")
-    print(updated_data[primary_key_columns])
+# プライマリキーのカラム名
+primary_key_columns = ['MACHINESN', 'PLANTID']
+
+add_key = list(set(internal_data["key"]) - set(external_data["key"]))
+del_key = list(set(external_data["key"]) - set(internal_data["key"]))
+
+# 追加データ
+add_data = internal_data[internal_data["key"].isin(add_key)]
+# 削除データ
+del_data = external_data[external_data["key"].isin(del_key)]
+# 更新データ
+update_data = pd.merge(internal_data, external_data, on=primary_key_columns, how="inner", suffixes=("_internal", "_external"))
+update_data = update_data[update_data["_internal"].ne(update_data["_external"])]
+
+diff_data = {table_name: {"ADD": add_data[col_list].to_dict(orient="records"), "DEL": del_data[col_list].to_dict(orient="records"), "UPDATE": update_data[col_list].to_dict(orient="records")}}
+
+pprint(diff_data)
