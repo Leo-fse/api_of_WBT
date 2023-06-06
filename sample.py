8de1d3998316cd43ba4072046410037e587af037
Tomoki Morimoto <morimoto.ke@gmail.com>
2023-06-06 11:37:39 +0900

    ⚡️

--- sample.py
+++ sample.py
@@ -24,23 +24,26 @@ machine_sn_data_external = cursor.fetchall()
 # プライマリキーのカラム名
 primary_key_columns = ['MachineSN', 'PlantID']
 
+# カラム名の取得
+columns_internal = [column[0] for column in cursor.description]
+
 # 新規追加データの検出
 for row_external in machine_sn_data_external:
-    primary_key_external = tuple(row_external[i] for i, column in enumerate(machine_sn_data_external.description) if column[0] in primary_key_columns)
+    primary_key_external = tuple(row_external[columns_internal.index(column)] for column in primary_key_columns)
 
-    if primary_key_external not in [tuple(row_internal[i] for i, column in enumerate(machine_sn_data_internal.description) if column[0] in primary_key_columns) for row_internal in machine_sn_data_internal]:
+    if primary_key_external not in [tuple(row_internal[columns_internal.index(column)] for column in primary_key_columns) for row_internal in machine_sn_data_internal]:
         print(f"新規追加: MachineSN={primary_key_external[0]}, PlantID={primary_key_external[1]}")
 
 # 更新データの検出
 for row_external in machine_sn_data_external:
-    primary_key_external = tuple(row_external[i] for i, column in enumerate(machine_sn_data_external.description) if column[0] in primary_key_columns)
-    external_columns = tuple(row_external[i] for i, column in enumerate(machine_sn_data_external.description) if column[0] not in primary_key_columns)
+    primary_key_external = tuple(row_external[columns_internal.index(column)] for column in primary_key_columns)
+    external_columns = tuple(row_external[i] for i, column in enumerate(columns_internal) if column not in primary_key_columns)
 
-    internal_rows = [row_internal for row_internal in machine_sn_data_internal if tuple(row_internal[i] for i, column in enumerate(machine_sn_data_internal.description) if column[0] in primary_key_columns) == primary_key_external]
+    internal_rows = [row_internal for row_internal in machine_sn_data_internal if tuple(row_internal[columns_internal.index(column)] for column in primary_key_columns) == primary_key_external]
 
     if internal_rows:
         row_internal = internal_rows[0]
-        internal_columns = tuple(row_internal[i] for i, column in enumerate(machine_sn_data_internal.description) if column[0] not in primary_key_columns)
+        internal_columns = tuple(row_internal[i] for i, column in enumerate(columns_internal) if column not in primary_key_columns)
 
         if external_columns != internal_columns:
             print(f"更新データ: MachineSN={primary_key_external[0]}, PlantID={primary_key_external[1]}")
