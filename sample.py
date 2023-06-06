42461849b3e7cea39e065954367e24ead7a6f2bf
Tomoki Morimoto <morimoto.ke@gmail.com>
2023-06-06 16:11:12 +0900

    ⚡️

--- sample.py
+++ sample.py
@@ -3,6 +3,7 @@ import cx_Oracle
 import os
 import configparser
 
+
 def get_data(username, password, host, port, service_name, table_name):
     dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
     connection = cx_Oracle.connect(username, password, dsn)
@@ -10,6 +11,7 @@ def get_data(username, password, host, port, service_name, table_name):
     data = pd.read_sql(query, connection)
     connection.close()
     return data
+
 
 def compare_tables(internal_config, external_config, table_name, primary_key_columns):
     # 内部版データベース接続情報
@@ -27,9 +29,11 @@ def compare_tables(internal_config, external_config, table_name, primary_key_col
     external_service_name = external_config["SERVICE_NAME"]
 
     # 社内版データの取得
-    internal_data = get_data(internal_username, internal_password, internal_host, internal_port, internal_service_name, table_name)
+    internal_data = get_data(internal_username, internal_password, internal_host, internal_port, internal_service_name,
+                             table_name)
     # 社外版データの取得
-    external_data = get_data(external_username, external_password, external_host, external_port, external_service_name, table_name)
+    external_data = get_data(external_username, external_password, external_host, external_port,
+                             external_service_name, table_name)
 
     internal_col_list = list(internal_data.columns)
     external_col_list = list(external_data.columns)
@@ -49,7 +53,8 @@ def compare_tables(internal_config, external_config, table_name, primary_key_col
     # 削除データ
     del_data = external_data[external_data["key"].isin(del_key)]
     # 更新データ
-    update_data = pd.concat([internal_data, external_data, add_data, del_data]).drop_duplicates(subset=col_list, keep=False)
+    update_data = pd.concat([internal_data, external_data, add_data, del_data]).drop_duplicates(subset=col_list,
+                                                                                              keep=False)
 
     diff_data = {
         table_name: {
@@ -58,8 +63,9 @@ def compare_tables(internal_config, external_config, table_name, primary_key_col
             "UPDATE": update_data[col_list].to_dict(orient="records")
         }
     }
-    
+
     return diff_data
+
 
 # 設定ファイルから接続情報を読み込む
 config = configparser.ConfigParser()
@@ -69,16 +75,25 @@ if os.path.isfile(ini_file_path):
 else:
     raise FileNotFoundError(f"iniファイルが存在しません")
 
-# テーブル名とプライマリキーの指定
-table_name = 'MachineSN'
-primary_key_columns = ['MACHINESN', 'PLANTID']
+target = [
+    {"table_name": "MACHINESN", "primary_key": ["MACHINESN", "PLANTID"]},
+    # 他のテーブルに関する情報も追加できます
+]
 
-# 社内版データベースと社外版データベースの接続情報を取得
-internal_config = config["INTERNAL"]
-external_config = config["EXTERNAL"]
+diff_data = {}
 
-# テーブルのデータを比較
-diff_data = compare_tables(internal_config, external_config, table_name, primary_key_columns)
+for table_info in target:
+    table_name = table_info["table_name"]
+    primary_key_columns = table_info["primary_key"]
+
+    # 社内版データベースと社外版データベースの接続情報を取得
+    internal_config = config["INTERNAL"]
+    external_config = config["EXTERNAL"]
+
+    # テーブルのデータを比較
+    table_diff = compare_tables(internal_config, external_config, table_name, primary_key_columns)
+
+    diff_data.update(table_diff)
 
 # 結果の出力
 pprint(diff_data)
